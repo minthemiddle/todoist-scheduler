b6c5996c43520d6eb85e89ecf6b544a049063702
Valia Walsk <walsk@yandex.com>
2023-04-06 18:01:32 +0300

    switches to .conf + dynamic label names + removes default filtering by project

--- todoist.py
+++ todoist.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
 
 # Standard library imports.
-import os
 import sys
+import configparser
 # Third-party imports.
 import requests
-from dotenv import load_dotenv
-# No local imports.
 
 
-def get_tasks(token, tasks_url, day, project, label):
-    filter_string = '{} & @{} & ##{}'.format(day, label, project)
+def read_config():
+    config = configparser.ConfigParser()
+    config.read(".todoist_scheduler.conf")
+    return config["todoist"]
+
+
+def get_labels(token, labels_url):
     auth_string = "Bearer {}".format(token)
-    response = requests.get(tasks,
-                            params={"filter": filter_string},
+    response = requests.get(labels_url,
                             headers={"Authorization": auth_string})
     return response.json()
 
 
-def count_tasks(i5,i15,i30,i45):
-    return len(i5)+len(i15)+len(i30)+len(i45)
+def get_tasks(token, tasks_url, day, label_id):
+    auth_string = "Bearer {}".format(token)
+    response = requests.get(tasks_url,
+                            params={"filter": day, "label": label_id},
+                            headers={"Authorization": auth_string})
+
+    return response.json()
 
 
-def sum_tasks(i5, i15, i30, i45):
-    s5 = len(i5) * 5
-    s15 = len(i15) * 15
-    s30 = len(i30) * 30
-    s45 = len(i45) * 45
-    total_seconds = (s5 + s15 + s30 + s45)*60
+def count_tasks(task_dict):
+    return sum(len(tasks) for tasks in task_dict.values())
 
+
+def sum_tasks(task_dict):
+    total_seconds = sum(len(tasks) * int(duration) * 60 for duration, tasks in task_dict.items())
     hours = total_seconds // 3600
     minutes = total_seconds // 60 - hours * 60
 
@@ -37,24 +43,34 @@ def sum_tasks(i5, i15, i30, i45):
 
 
 if __name__ == "__main__":
-    # Collect script variables from user.
-    load_dotenv()
-    your_token = os.getenv('TODOIST_KEY', '')
+    # Read config and get API key and default project name.
+    config = read_config()
+    your_token = config.get("api_key", "")
+    default_project_name = config.get("default_project_name", "")
+
     if not your_token:
-        print("No API Key, did you set 'TODOIST_KEY' environment variable?")
+        print("No API Key, did you set 'api_key' in .todoist_scheduler.conf?")
         sys.exit(1)
-    tasks = 'https://api.todoist.com/rest/v2/tasks'
-    project = input('Project: ')
-    day = input('Day: ')
+
+    tasks_url = 'https://api.todoist.com/rest/v2/tasks'
+    labels_url = 'https://api.todoist.com/rest/v2/labels'
+    day = "today"   # Bcs usually it would be today
+
+    # Get labels from Todoist API.
+    labels = get_labels(your_token, labels_url)
+
+    # Get tasks from Todoist API.
+    # Detect labels like "25min".
+    time_labels = {label["name"][:-3]: label["name"] for label in labels if label["name"].endswith("min") and label["name"][:-3].isdigit()}
 
     # Collect tasks from Todoist API.
-    m5 = get_tasks(your_token, tasks, day, project, label="m5")
-    m15 = get_tasks(your_token, tasks, day, project, label="m15")
-    m30 = get_tasks(your_token, tasks, day, project, label="m30")
-    m45 = get_tasks(your_token, tasks, day, project, label="m45")
+    tasks_by_duration = {duration: get_tasks(your_token, tasks_url, day, label) for duration, label in time_labels.items()}
+    for duration, tasks in tasks_by_duration.items():
+        print("Duration: {}min".format(duration))
+        for task in tasks:
+            print("  {}".format(task["content"]))
 
     # Summarize tasks for user.
-    print("Task Count: {}".format(count_tasks(m5,m15,m30,m45)))
-    print("Estimated Time: {}".format(sum_tasks(m5,m15,m30,m45)))
-
+    print("Task Count: {}".format(count_tasks(tasks_by_duration)))
+    print("Estimated Time: {}".format(sum_tasks(tasks_by_duration)))
 
